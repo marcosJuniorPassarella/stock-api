54133cd59d3cc1274feed8625c3c6f4f0bf859ad
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-02-27 23:01:32 -0300

    Added new packages

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import express, { Request, Response, NextFunction } from "express";
-
+import cors from "cors";
+import "express-async-errors";
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use(router);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({
+            error: err.message,
+        });
+    }
+    return res.status(500).json({
+        status: "error",
+        message: "Internal server error.",
+    });
+});
 
 app.listen(3333, () =>
     console.log("Servidor rodando na porta 3333 - BarberPro")
