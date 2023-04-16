464cbe30ad472001dbdaac40b32d81115ed42497
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-04-16 15:44:40 -0300

    Added dev script and rename project name

--- src/server.ts
+++ src/server.ts
@@ -20,5 +20,5 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 });
 
 app.listen(3333, () =>
-    console.log("Servidor rodando na porta 3333 - BarberPro")
+    console.log("Servidor rodando na porta 3333")
 );
