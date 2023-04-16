316303abbe4086757af35fa82a003d8239763f47
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-04-16 16:24:42 -0300

    Implement /user route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+
+/* Controllers */
+import { CreateUserController } from "./controllers/user/CreateUserController";
 
 const router = Router();
 
-router.get("/test", (req: Request, res: Response) => {
-    return res.json({ ok: true });
-});
+router.post("/user", new CreateUserController().handle);
 
 export { router };
