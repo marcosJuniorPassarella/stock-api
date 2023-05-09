eebbbd0f4973d4d94742d9d0a0d5e0c260e123a7
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-05-09 19:59:51 -0300

    Added validation

--- src/services/category/DeleteCategoryService.ts
+++ src/services/category/DeleteCategoryService.ts
@@ -3,12 +3,20 @@ import prismaClient from "../../prisma/index";
 class DeleteCategoryService {
   async execute(category_id: string) {
     if (category_id) {
-      const category = await prismaClient.category.delete({
+      const categories = await prismaClient.category.findFirst({
         where: {
           id: category_id,
         },
       });
-      return category;
+
+      if (categories) {
+        const category = await prismaClient.category.delete({
+          where: {
+            id: category_id,
+          },
+        });
+        return category;
+      }
     }
   }
 }
